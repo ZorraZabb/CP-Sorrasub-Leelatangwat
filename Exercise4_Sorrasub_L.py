30a84dcf318ed0edc518127f7217d0f7aa2381c3
ZorraZabb <gagpoohpooh@gmail.com>
2021-02-22 14:46:40 +0700

    add Exercise

---
+++ Exercise4_Sorrasub_L.py
@@ -0,0 +1,9 @@
+print('--Your Score--')
+FE_score = 3.0
+GB_score = 4.0
+ICS_score = 2.5
+CP_score = 1.0
+print('Foundation English :',FE_score)
+print('General Business :',GB_score)
+print('Introduction to Computer Systems :',ICS_score)
+print('Computer Programming :',CP_score)
